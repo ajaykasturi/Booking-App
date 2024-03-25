1ddf95e7ea013387643a5b98b66c2c656c5dfd42
Ajay <ajayreigns0@gmail.com>
2024-03-26 00:34:20 +0530

    backend setup commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
+mongoose.connect(process.env.MONGODB_URL as string);
 const app = express();
 const port = 3002;
 
