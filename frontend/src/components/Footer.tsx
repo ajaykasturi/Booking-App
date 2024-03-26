13396ef50afe7aba2e3db0954db1c294d89bb56f
Ajay <ajayreigns0@gmail.com>
2024-03-26 11:18:26 +0530

    frontend landing layout page

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div className="bg-blue-800 py-10">
+      <div className="container mx-auto flex flex-row justify-between items-center">
+        <span className="text-white text-3xl font-bold tracking-tight">
+          Booking.com
+        </span>
+        <span className="text-white font-bold tracking-tight flex gap-4">
+          <p className="curson-pointer">Privacy Policy</p>
+          <p className="curson-pointer">Terms of Service</p>
+        </span>
+      </div>
+    </div>
+  );
+};
+export default Footer;
