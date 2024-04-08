508a8e561cc3dfa5ca68e22843be4b3b79b30168
Ajay <ajayreigns0@gmail.com>
2024-04-08 23:08:30 +0530

    backend of search and pagination

--- backend/src/routes/apis.ts
+++ backend/src/routes/apis.ts
@@ -2,10 +2,11 @@ import express from "express";
 import userRouter from "./users";
 import authRouter from "./auth";
 import myHotelRouter from "./my-hotels";
+import hotelsRouter from "./hotels";
 const router = express.Router();
 
 router.use("/users", userRouter);
 router.use("/auth", authRouter);
 router.use("/my-hotels", myHotelRouter);
-
+router.use("/hotels", hotelsRouter);
 export default router;
