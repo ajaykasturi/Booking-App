a9a7ccb1b3d3f1e285aacb59bfab43472e6964e4
Ajay <52690028+ajaykasturi@users.noreply.github.com>
2024-05-01 18:53:16 +0530

    Update Search.tsx

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -66,7 +66,7 @@ const Search = () => {
   };
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
-      <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+      <div className="rounded-lg border border-slate-300 p-5 h-fit lg:sticky top-10">
         <div className="space-y-5">
           <h3 className="font-semibold text-lg border-b border-slate-300 pb-5">
             Filter By:
